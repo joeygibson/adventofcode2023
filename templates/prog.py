916069b224bd6c559a85a64d3f2ef58958565cb9
Joey Gibson <joey@joeygibson.com>
2023-12-01 17:07:55 -0500

    convenience script

---
+++ templates/prog.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def part1(lines: list[str]) -> int:
+    pass
+
+
+def part2(lines: list[str]) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as f:
+        lines = f.read().splitlines()
+
+    print(f'part1 -> {part1(lines)}')
+    print(f'part2 -> {part2(lines)}')
