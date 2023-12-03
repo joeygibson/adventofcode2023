21936cde747e9bb0e43a7b8909aa515b848258d9
Joey Gibson <joey@joeygibson.com>
2023-12-03 12:10:05 -0500

    day 3, part 2

--- day3/prog.py
+++ day3/prog.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import functools
 import re
 import sys
+from collections import defaultdict
 from typing import Tuple
+
+popular_symbols: dict[str, list[int]] = defaultdict(list)
 
 
 def find_symbols(lines: list[str]) -> list[Tuple[str, int, int, int]]:
@@ -26,14 +30,21 @@ def find_numbers(lines: list[str]) -> list[Tuple[int, int, int, int]]:
     return numbers
 
 
-def is_num_near_symbol(num: Tuple[int, int, int, int], symbols: list[Tuple[str, int, int, int]]) -> bool:
-    for sym in symbols:
-        if num[2] <= sym[2] <= num[3]:
-            return True
-        if sym[2] == num[2] - 1 or sym[3] == num[3] + 1:
-            return True
+def is_thing_near_another_thing(thing: Tuple[int, int, int, int],
+                                other_things: list[Tuple[str, int, int, int]]) -> bool:
+    for other_thing in other_things:
+        found = False
 
-    return False
+        if thing[2] <= other_thing[2] <= thing[3]:
+            found = True
+        if other_thing[2] == thing[2] - 1 or other_thing[3] == thing[3] + 1:
+            found = True
+
+        if other_thing[0] == '*' and found:
+            popular_symbols[other_thing].append(thing)
+            break
+
+    return found
 
 
 def find_numbers_near_symbols(symbols: list[Tuple[str, int, int, int]],
@@ -45,15 +56,15 @@ def find_numbers_near_symbols(symbols: list[Tuple[str, int, int, int]],
         next_row_symbols = [s for s in symbols if s[1] == num[1] + 1]
         cur_row_symbols = [s for s in symbols if s[1] == num[1]]
 
-        if prev_row_symbols and is_num_near_symbol(num, prev_row_symbols):
+        if prev_row_symbols and is_thing_near_another_thing(num, prev_row_symbols):
             vals.append(num[0])
             continue
 
-        if next_row_symbols and is_num_near_symbol(num, next_row_symbols):
+        if next_row_symbols and is_thing_near_another_thing(num, next_row_symbols):
             vals.append(num[0])
             continue
 
-        if cur_row_symbols and is_num_near_symbol(num, cur_row_symbols):
+        if cur_row_symbols and is_thing_near_another_thing(num, cur_row_symbols):
             vals.append(num[0])
             continue
 
@@ -69,7 +80,16 @@ def part1(lines: list[str]) -> int:
 
 
 def part2(lines: list[str]) -> int:
-    pass
+    vals = list(filter(lambda x: len(x) == 2, popular_symbols.values()))
+    print(vals)
+
+    for i in range(len(vals)):
+        vals[i] = list(map(lambda x: x[0], vals[i]))
+
+    for i in range(len(vals)):
+        vals[i] = functools.reduce(lambda x, y: x * y, vals[i])
+
+    return sum(vals)
 
 
 if __name__ == '__main__':
