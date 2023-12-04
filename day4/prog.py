15b6e00e6d4f7b99474a96a41aaa220857476b22
Joey Gibson <joey@joeygibson.com>
2023-12-04 13:41:51 -0500

    day 4, part 2

--- day4/prog.py
+++ day4/prog.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+import queue
+from dataclasses import dataclass
 import sys
 
 
@@ -22,14 +23,57 @@ def part1(lines: list[str]) -> int:
         if card[1] == 1:
             results.append(1)
         else:
-            results.append(2**(card[1]-1))
+            results.append(2 ** (card[1] - 1))
 
     return sum(results)
 
 
+@dataclass
+class Card:
+    name: str
+    winning_numbers: list[str]
+    my_numbers: list[str]
+
+    def get_matches(self) -> set[str]:
+        return set(self.winning_numbers).intersection(set(self.my_numbers))
+
+    def get_card_number(self) -> int:
+        return int(self.name.removeprefix('Card').strip())
+
 
 def part2(lines: list[str]) -> int:
-    pass
+    cards: dict[str, Card] = {}
+
+    for line in lines:
+        chunks = line.split(': ')
+        raw_winning_numbers, raw_my_numbers = chunks[1].split(' | ')
+        winning_numbers = raw_winning_numbers.split()
+        my_numbers = raw_my_numbers.split()
+
+        name_chunks = chunks[0].split()
+        card_name = f'Card {name_chunks[1]}'
+        cards[card_name] = Card(card_name, winning_numbers, my_numbers)
+
+    total_cards: int = 0
+
+    card_stash = queue.Queue()
+    for card in cards.values():
+        card_stash.put(card)
+
+    while not card_stash.empty():
+        card = card_stash.get()
+        total_cards += 1
+
+        matches = card.get_matches()
+
+        for i in range(1, len(matches) + 1):
+            card_number = card.get_card_number()
+            new_card_number = f'Card {card_number + i}'
+            new_card = cards.get(new_card_number)
+            if new_card:
+                card_stash.put(new_card)
+
+    return total_cards
 
 
 if __name__ == '__main__':
