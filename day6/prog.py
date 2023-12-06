18a99f9c29088b633478c7f8dc684486cba836fd
Joey Gibson <joey@joeygibson.com>
2023-12-06 17:10:22 -0500

    day 6, part 2, quadratic

--- day6/prog.py
+++ day6/prog.py
@@ -2,6 +2,7 @@
 import functools
 import sys
 import unittest
+from math import floor, sqrt
 
 
 def process_races(durations: list[int], records: list[int]) -> list[int]:
@@ -20,6 +21,11 @@ def process_races(durations: list[int], records: list[int]) -> list[int]:
     return possibilities
 
 
+def process_part2(duration: int, record: int) -> list[int]:
+    return len(range(floor((duration - sqrt(duration ** 2 - 4 * record)) / 2),
+                     floor((duration + sqrt(duration ** 2 - 4 * record)) / 2)))
+
+
 def part1(lines: list[str]) -> int:
     durations = [int(d) for d in lines[0].split(':')[1].split()]
     records = [int(d) for d in lines[1].split(':')[1].split()]
@@ -33,10 +39,8 @@ def part2(lines: list[str]) -> int:
     duration = int(lines[0].split(':')[1].replace(' ', ''))
     record = int(lines[1].split(':')[1].replace(' ', ''))
 
-    possibilities = process_races([duration], [record])
-
-    return functools.reduce(lambda x, y: x * y, possibilities)
-
+    return process_part2(duration, record)
+    
 
 class TestDay6(unittest.TestCase):
     def test_part1(self):
