cf89a0d35362268021114dec25a583cbcea47174
Joey Gibson <joey@joeygibson.com>
2023-12-16 23:22:24 -0500

    day 15, part 2

--- day15/prog.py
+++ day15/prog.py
@@ -2,6 +2,18 @@
 
 import sys
 import unittest
+from dataclasses import dataclass
+
+
+def hash(item: str) -> str:
+    current_value = 0
+
+    for val, ord_val in [(l, ord(l.strip())) for l in item]:
+        current_value += ord_val
+        current_value *= 17
+        current_value = current_value % 256
+
+    return current_value
 
 
 def part1(data: str) -> int:
@@ -9,20 +21,55 @@ def part1(data: str) -> int:
 
     total = 0
     for item in chunks:
-        current_value = 0
-
-        for val, ord_val in [(l, ord(l.strip())) for l in item]:
-            current_value += ord_val
-            current_value *= 17
-            current_value = current_value % 256
+        current_value = hash(item)
 
         total += current_value
 
     return total
 
 
+@dataclass
+class Lens:
+    label: str
+    focal_length: int
+
+
 def part2(data: str) -> int:
-    pass
+    chunks = data.split(',')
+    boxes = {}
+
+    for instruction in chunks:
+        if '=' in instruction:
+            label, value = instruction.split('=')
+            box_id = hash(label)
+            lens = Lens(label, int(value))
+
+            box = boxes.get(box_id, [])
+
+            if any([l for l in box if l.label == label]):
+                lens_index = next((i for i, l in enumerate(box) if l.label == label), -1)
+                box[lens_index] = lens
+            else:
+                box.append(lens)
+        else:
+            label = instruction.strip('-')
+            box_id = hash(label)
+            box = boxes.get(box_id, [])
+
+            if any([l for l in box if l.label == label]):
+                lens_index = next((i for i, l in enumerate(box) if l.label == label), -1)
+                box.pop(lens_index)
+
+        boxes[box_id] = box
+
+    total = 0
+
+    for box_id, box in boxes.items():
+        for pos, lens in enumerate(box):
+            focus_power = (1 + int(box_id)) * (pos + 1) * lens.focal_length
+            total += focus_power
+
+    return total
 
 
 class TestProg(unittest.TestCase):
@@ -35,12 +82,12 @@ class TestProg(unittest.TestCase):
         self.assertEqual(52, res)
 
     def test_part1(self):
-
         res = part1(self.data)
         self.assertEqual(1320, res)
 
     def test_part2(self):
         res = part2(self.data)
+        self.assertEqual(145, res)
 
 
 if __name__ == '__main__':
