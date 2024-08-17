a751162c4794b749ae61a2640f3e0ccaf4295b17
Joey Gibson <joey@joeygibson.com>
2024-08-16 21:32:52 -0400

    day 5, part 2 - lisp - doesn't work

--- day05/prog.lisp
+++ day05/prog.lisp
@@ -28,7 +28,8 @@
 
 (defun print-things (thing-map)
   (maphash (lambda (k v)
-             (format t "~&~a: ~a~%" k v)) thing-map))
+             (format t "~&~a: ~a~%" k v))
+           thing-map))
 
 (defun create-maps (sections)
   (let* ((maps (make-hash-table :test #'equal)))
@@ -62,9 +63,47 @@
 (format t "~&part1: ~a~%"  (part1 "input0.txt"))
 (format t "~&part1: ~a~%"  (part1 "input1.txt"))
 
+(defun partition (lst)
+  (cons (list (first lst) (second lst))
+        (if (cddr lst)
+            (partition (cddr lst)))))
+
+(defun remap (start end seeds map)
+  (let* ((new-seeds nil))
+    (loop for (dest-range-start source-range-start range-length) in map
+          do (let* ((overlap-start (max start source-range-start))
+                    (overlap-end (min end (+ source-range-start range-length))))
+               (if (< overlap-start overlap-end)
+                   (progn
+                     (push (list (+ (dest-range-start (- overlap-start source-range-start)))
+                                 (+ (dest-range-start (- overlap-end source-range-start))))
+                           new-seeds)
+                     (if (< start overlap-start)
+                         (push ))))))))
 
 
+(defun part2 (file-name)
+  (let* ((lines (uiop:read-file-lines file-name))
+         (seeds (mapcar (lambda (pair)
+                          (list (first pair)
+                                (+ (first pair)
+                                   (second pair))))
+                        (partition (mapcar #'parse-integer
+                                          (rest (cl-ppcre:split "\\s+" (first lines)))))))
+         (sections (split-sequence:split-sequence-if
+                    (lambda (line) (eql (length line) 0))
+                    (cddr lines)))
+         (maps (mapcar (lambda (section)
+                         (mapcar (lambda (line)
+                                   (mapcar #'parse-integer (cl-ppcre:split "\\s+" line)))
+                                 (rest section)))
+                       sections)))
+    (loop for m in maps
+          do (let* ((new-seeds nil))
+               (loop for (start end) in seeds if (and start end)
+                     do ())))))
 
+(print (part2 "input0.txt"))
 
 
 
