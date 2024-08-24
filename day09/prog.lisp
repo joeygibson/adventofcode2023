0b989eb080e6856570a4e2a4aa3598deddd5d3e5
Joey Gibson <joey@joeygibson.com>
2024-08-24 11:17:07 -0400

    day 9, part 1 - lisp - partial

--- day09/prog.lisp
+++ day09/prog.lisp
@@ -3,6 +3,8 @@
 (ql:quickload :cl-ppcre)
 (use-package :cl-ppcre)
 
+(setf *print-circle* t)
+
 (defun pairwise (list)
   (loop for (a b) on list
         while b
@@ -18,30 +20,32 @@
 (defun solve (lines)
   (let ((results nil))
     (mapc (lambda (line)
+            (print "-----")
             (let ((sequences (make-array 5 :fill-pointer 0))
                   (values (loop for val in (all-matches-as-strings "\\d+" line)
                                 collecting (parse-integer val))))
               (vector-push values sequences)
               (loop named diffs
                     do (let ((differences (compute-differences values)))
+                         (vector-push differences sequences)
                          (if (every (lambda (v) (= v 0))
                                     differences)
                              (return-from diffs))
                          (setf values differences)))
               (let ((rev-sequences (reverse sequences)))
+                (format t "~&rs: ~a~%" rev-sequences)
                 (loop for i from 0
-                      for sequence across sequences
-                      do (if (not (= i 0))
-                             (vector-push (+ (aref sequence (1- (length sequence)))
-                                             (let ((s (aref rev-sequences (1- i))))
-                                               (aref s (1- (length s))))))
-                             (vector-push (aref sequence (1- (length sequence))))))
-                (vector-push (let ((s (aref sequences 0)))
-                               (aref s (1- (length s))))))))
+                      for sequence across rev-sequences
+                      do (progn
+                           (format t "~&~a ~a~%" i sequence)
+                           (if (not (= i 0))
+                               (nconc sequence (list (+ (car (last sequence))
+                                                        (car (last (aref rev-sequences (1- i)))))))
+                               (nconc sequence (list (car (last sequence)))))))
+                (nconc results (let ((s (aref sequences 0)))
+                                 (car (last s)))))))
           lines)
     (reduce #'+ results)))
-
-
 
 (defun part1 (file-name)
   (let ((lines (uiop:read-file-lines file-name)))
@@ -60,3 +64,11 @@
       do (if (= i 5)
              (return-from foo 23)
              (print i)))
+
+(let ((l (list 1 2 3)))
+  (nconc l (list 23))
+  (print l))
+
+(let ((l (list 1 2 3)))
+  (nth 1 l))
+
