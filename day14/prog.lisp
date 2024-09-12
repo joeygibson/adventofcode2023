ec0da00dbfcbbbb2dbcd2176e32ebab76aa1385d
Joey Gibson <joey@joeygibson.com>
2024-09-12 18:52:46 -0400

    day 14, part 2 - lisp - partial

--- day14/prog.lisp
+++ day14/prog.lisp
@@ -7,7 +7,7 @@
 
 (use-package :lisp-utils)
 (use-package :cl-ppcre)
-(use-package :alexandria)
+;; (use-package :alexandria)
 
 (setf *print-circle* t)
 
@@ -21,7 +21,7 @@
     map))
 
 (defun find-empty-row (map row-num col-num)
-  (let ((row-range (reverse (iota row-num)))
+  (let ((row-range (reverse (alexandria:iota row-num)))
         (row-to-move-to row-num))
     (loop for i in row-range
           until (or (equal (gethash (cons i col-num) map) "#")
@@ -31,21 +31,36 @@
 
 (defun tilt (map)
   (let ((pairs (remove-if (lambda (x)
-                            (let ((val (cdr x)))
-                              (or (equal (car x) 0)
-                                  (equal val "#")
-                                  (equal val "."))))
-                          (reverse (hash-table-keys map)))))
+                            (or (equal (car x) 0)
+                                (or (equal (gethash x map) "#")
+                                    (equal (gethash x map) "."))))
+                          (reverse (alexandria:hash-table-keys map)))))
     (loop for pair in pairs
           do (let* ((row-num (car pair))
                     (col-num (cdr pair))
                     (row-to-move-to (find-empty-row map row-num col-num)))
-               (format t "~&~a -> ~a~%" row-num row-to-move-to)
                (if (not (equal row-to-move-to row-num))
                    (progn
                      (setf (gethash (cons row-to-move-to col-num) map) "O")
                      (setf (gethash pair map) ".")))))
     map))
+
+(defun rotate (map cols)
+  (let ((rotated (make-hash-table :test #'equal)))
+    (maphash (lambda (key value)
+               (let* ((x (car key))
+                      (y (cdr key))
+                      (new-x y)
+                      (new-y (- (1- cols) x)))
+                 (setf (gethash (cons new-x new-y) rotated) value)))
+             map)
+    rotated))
+
+(defun cycle (map cols)
+  (let ((tilted-map nil))
+    (loop for i below 4
+          do (setf tilted-map (rotate (tilt map) cols)))
+    tilted-map))
 
 (defun part1 (file-name)
   (let* ((lines (uiop:read-file-lines file-name))
@@ -54,10 +69,6 @@
          (map (create-map lines))
          (tilted-map (tilt map))
          (total 0))
-    ;; (mapcar (lambda (a b)
-    ;;           (format t "~&~a - ~a~%" a b))
-    ;;         (hash-table-alist map)
-    ;;         (hash-table-alist tilted-map))
     (loop for row-num below max-rows
         do (let ((rock-count (length (loop for col-num below max-cols
                                            if (equal (gethash (cons row-num col-num) tilted-map) "O")
@@ -65,12 +76,55 @@
              (setf total (+ total (* rock-count (- max-rows row-num))))))
     total))
 
-(print (part1 "input0.txt"))
+(defun deep-copy (map)
+  (let ((pairs (alexandria:hash-table-alist map)))
+    (alexandria:alist-hash-table pairs)))
 
-(let ((m (create-map (uiop:read-file-lines "input0.txt"))))
-  (loop for pair in (reverse (hash-table-keys m))
-        do (let* ((row-num (car pair))))
-           (print row-num)))
+(defun part2 (file-name)
+  (let* ((lines (uiop:read-file-lines file-name))
+         (rows (length lines))
+         (cols (length (car lines)))
+         (the-map (create-map lines))
+         (seen (list the-map))
+         (cycles 1000000000)
+         (loop-index 0)
+         (loop-length 0)
+         (final-map nil)
+         (total 0))
+    (loop for i below cycles
+          do 
+             (progn
+               (setf the-map (cycle (deep-copy the-map) cols))
+               (print i)
+               (when (member the-map seen :test #'equalp)
+                 (progn
+                   (print "hit!")
+                   (setf loop-index (position the-map seen :test #'equalp))
+                   (setf loop-length (- (1+ i) loop-index))
+                   (return)))
+               
+               (setf seen (append seen (list the-map)))))
+
+    (setf final-map (nth (+ (mod (- cycles loop-index) loop-length) loop-index) seen))
+    
+    (loop for row-num below rows
+          do (let ((rock-count (length (loop for col-num below cols
+                                             if (equal (gethash (cons row-num col-num) final-map) "O")
+                                               collect 1))))
+               (setf total (+ total (* rock-count (- rows row-num))))))
+    total))
+
+(print (part1 "input0.txt"))
+(print (part1 "input1.txt"))
+
+(print (part2 "input0.txt"))
+
+(let* ((m (create-map (uiop:read-file-lines "input0.txt")))
+       (rm (rotate m 10))
+       (rm1 (rotate m 10))
+       (seen (list m rm)))
+  (member rm seen :test #'equalp))
+
 
 
 
